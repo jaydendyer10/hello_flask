51fc05c90a5eb924da3fd3a562588b5af1c2dbf5
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-09-28 16:48:57 -0600

    single guide id

--- app.py
+++ app.py
@@ -63,5 +63,13 @@ def guide_update(id):
     db.session.commit()
     return guide_schema.jsonify(guide)
 
+@app.route("/guide/<id>", methods=["DELETE"])
+def guide_delete(id:
+    guide = Guide.query.get(id)
+    db.session.delete(guide)
+    db.session.commot()
+
+    return guide_schema.jsonify(guide)
+
 if __name__ == '__main__':
     app.run(debug = True)
