52ea48e954306a74db79d98433d5b6dede24d4a2
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-09-28 16:02:02 -0600

    added single guide

--- app.py
+++ app.py
@@ -46,5 +46,10 @@ def get_guides():
     result = guides_schema.dump(all_guides)
     return jsonify(result)
 
+@app.route("/guide/<id>", methods =["GET"])
+def get_guide(id):
+    guide = Guide.query.get(id)
+    return guide_schema.jsonify(guide)
+
 if __name__ == '__main__':
     app.run(debug = True)
